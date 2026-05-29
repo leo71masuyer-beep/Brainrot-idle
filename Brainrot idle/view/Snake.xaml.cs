18d47991a5ab8cb7401ed64669838d8ca749b14c
Léo <leo71.masuyer@gmail.com>
2026-05-29 08:17:07 +0200

    Merge branch 'master' of https://github.com/leo71masuyer-beep/Brainrot-idleamélioration snake

--- Brainrot idle/view/Snake.xaml.cs
+++ Brainrot idle/view/Snake.xaml.cs
@@ -15,17 +15,22 @@ namespace Brainrot_idle.view
         private System.Windows.Threading.DispatcherTimer timerJeu = new System.Windows.Threading.DispatcherTimer();
         private int directionX = 1;
         private int directionY = 0;
+        private Rectangle nourriture;
+        private Random rnd = new Random();
 
         public Snake()
         {
             InitializeComponent();
-            AjouterMorceau(200, 200);
 
-            timerJeu.Interval = TimeSpan.FromMilliseconds(150);
+            // On crée la tête
+            AjouterMorceau(200, 200);
+            //On fait apparaître la première pomme
+            PlacerNourriture();
+
+            timerJeu.Interval = TimeSpan.FromMilliseconds(50);
             timerJeu.Tick += TimerJeu_Tick;
             timerJeu.Start();
 
-            // forcer le focus pour écouter le clavier
             this.Focusable = true;
             this.Loaded += (s, e) => this.Focus();
         }
@@ -38,12 +43,19 @@ namespace Brainrot_idle.view
         private void TimerJeu_Tick(object sender, EventArgs e)
         {
             BougerSerpent();
+            VerifierCollisionNourriture();
         }
 
         private void BougerSerpent()
         {
-            Rectangle tete = corpsDuSerpent[0];
+            // Chaque morceau prend la place de celui qui le précède
+            for (int i = corpsDuSerpent.Count - 1; i > 0; i--)
+            {
+                Canvas.SetLeft(corpsDuSerpent[i], Canvas.GetLeft(corpsDuSerpent[i - 1]));
+                Canvas.SetTop(corpsDuSerpent[i], Canvas.GetTop(corpsDuSerpent[i - 1]));
+            }
 
+            Rectangle tete = corpsDuSerpent[0];
             double positionXActuelle = Canvas.GetLeft(tete);
             double positionYActuelle = Canvas.GetTop(tete);
 
@@ -51,13 +63,58 @@ namespace Brainrot_idle.view
             Canvas.SetTop(tete, positionYActuelle + (directionY * TailleCase));
         }
 
+        // Placer la nourriture aléatoirement
+        private void PlacerNourriture()
+        {
+            if (nourriture != null) ZoneDeJeu.Children.Remove(nourriture);
+
+            nourriture = new Rectangle
+            {
+                Width = TailleCase,
+                Height = TailleCase,
+                Fill = Brushes.Red
+            };
+
+            int x = rnd.Next(0, 20) * TailleCase;
+            int y = rnd.Next(0, 15) * TailleCase;
+
+            Canvas.SetLeft(nourriture, x);
+            Canvas.SetTop(nourriture, y);
+            ZoneDeJeu.Children.Add(nourriture);
+        }
+
+        // Vérifier si on mange la nourriture 
+        private void VerifierCollisionNourriture()
+        {
+            Rectangle tete = corpsDuSerpent[0];
+
+            double teteX = Canvas.GetLeft(tete);
+            double teteY = Canvas.GetTop(tete);
+            double nourritureX = Canvas.GetLeft(nourriture);
+            double nourritureY = Canvas.GetTop(nourriture);
+
+            // Si la tête est exactement sur la nourriture
+            if (teteX == nourritureX && teteY == nourritureY)
+            {
+                // On ajoute un nouveau morceau. 
+                // car la boucle dans BougerSerpent le replacera correctement au prochain mouvement.
+                AjouterMorceau(-50, -50);
+
+                // On déplace la nourriture ailleurs
+                PlacerNourriture();
+            }
+        }
+
         private void AjouterMorceau(double x, double y)
         {
+            // On peut optionnellement changer la couleur du corps pour le différencier de la tête
+            Brush couleur = corpsDuSerpent.Count == 0 ? Brushes.Green : Brushes.LightGreen;
+
             Rectangle morceau = new Rectangle
             {
                 Width = TailleCase,
                 Height = TailleCase,
-                Fill = Brushes.Green,
+                Fill = couleur,
                 Margin = new Thickness(1)
             };
 
@@ -70,22 +127,10 @@ namespace Brainrot_idle.view
 
         private void Page_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.Key == Key.Up && directionY != 1)
-            {
-                directionX = 0; directionY = -1;
-            }
-            else if (e.Key == Key.Down && directionY != -1)
-            {
-                directionX = 0; directionY = 1;
-            }
-            else if (e.Key == Key.Left && directionX != 1)
-            {
-                directionX = -1; directionY = 0;
-            }
-            else if (e.Key == Key.Right && directionX != -1)
-            {
-                directionX = 1; directionY = 0;
-            }
+            if (e.Key == Key.Up && directionY != 1) { directionX = 0; directionY = -1; }
+            else if (e.Key == Key.Down && directionY != -1) { directionX = 0; directionY = 1; }
+            else if (e.Key == Key.Left && directionX != 1) { directionX = -1; directionY = 0; }
+            else if (e.Key == Key.Right && directionX != -1) { directionX = 1; directionY = 0; }
         }
     }
 }
